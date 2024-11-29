54d617aee5d234052c339784d48cb98eeaf846c7
JonatV <mrangelsjon@gmail.com>
2024-11-29 15:10:03 +0100

    submit: cpp03/ex01 done

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,35 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+	protected:
+		std::string _name;
+		int _hitPoints;
+		int _hitPointsMax;
+		int _energyPoints;
+		int _attackDamage;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		~ClapTrap();
+
+		ClapTrap &operator=(const ClapTrap &src);
+
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+		const std::string& getName();
+		const int& getHp();
+		const int& getHpMax();
+		void displayHP();
+		void displayDMG();
+		void displayEnergy();
+		void displayName();
+
+};
+
+#endif
---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,68 @@
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap() : ClapTrap("ScavTrap4242")
+{
+	std::cout << "\e[2mDefault constructor ScavTrap called\e[0m" << std::endl;
+	this->_hitPoints = 100;
+	this->_hitPointsMax = 100;
+	this->_energyPoints = 50;
+	this->_attackDamage = 20;
+	this->_isGuard = false;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << "\e[2mParametrized constructor ScavTrap called\e[0m" << std::endl;
+	this->_hitPoints = 100;
+	this->_hitPointsMax = 100;
+	this->_energyPoints = 50;
+	this->_attackDamage = 20;
+	this->_isGuard = false;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
+{
+	std::cout << "Copy constructor ScavTrap called" << std::endl;
+	this->_isGuard = other._isGuard;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "\e[2mDestructor ScavTrap called\e[0m" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
+{
+	std::cout << "Assignation ScavTrap operator called" << std::endl;
+	this->_name = other._name;
+	this->_hitPoints = other._hitPoints;
+	this->_hitPointsMax = other._hitPointsMax;
+	this->_energyPoints = other._energyPoints;
+	this->_attackDamage = other._attackDamage;
+	return *this;
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+	displayName();
+	if (this->_hitPoints == 0)
+		std::cout << "\e[31mAttack denied. Crew is dead, no one can attack.\e[0m " << std::endl;
+	else if (this->_energyPoints > 0)
+	{
+		std::cout << "\e[32mDeal \e[42m" << _attackDamage << "\e[0;32m of damage to \e[0m[" << target << "]\e[0;32m.\e[0m" << std::endl;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << "\e[33mAttack denied. Not enought energy.\e[0m " << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	displayName();
+	this->_isGuard = !this->_isGuard;
+	if (this->_isGuard)
+		std::cout << "\e[32mEnabled Gate keeper mode.\e[0m" << std::endl;
+	else
+		std::cout << "\e[33mDisabled Gate keeper mode.\e[0m" << std::endl;
+}
