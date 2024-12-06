d8660a739f7d01583b5a3de0cffbfdf695afe979
JonatV <mrangelsjon@gmail.com>
2024-12-06 13:34:55 +0100

    submit: cpp05/ex01 done

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,58 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include "Bureaucrat.hpp"
+#include <string>
+
+class Form
+{
+	private:
+		static const int	_maxGrade = 1;
+		static const int	_minGrade = 150;
+		const std::string	_name;
+		const int			_executionGrade;
+		const int			_signedGrade;
+		bool				_isSigned;
+	public:
+	// Constructor
+		Form();
+
+	// Parameterized constructor
+		Form(std::string const &name, int executionGrade, int signedGrade);
+	// Copy constructor
+		Form(Form const &);
+		
+	// Destructor
+		~Form();
+		
+	// Overloads
+		Form	&operator=(Form const &);
+		
+	// Member functions
+		void	beSigned(const Bureaucrat &);
+		void	beExecuted(const Bureaucrat &);
+	// Getters
+		std::string const &getName() const;
+		bool	getIsSigned() const;
+		int		getSignedGrade() const;
+		int		getExecGrade() const;
+
+	// Setters
+		void	setIsSigned(bool);
+	// exceptions
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+	// friends
+		friend std::ostream	&operator<<(std::ostream &os, const Form &form);
+	
+};
+
+#endif
---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,54 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+#include <iostream>
+#include <string>
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	try {
+		Bureaucrat a;  // Calls default constructor
+		Bureaucrat b("Alice", 1);  // Calls parameterized constructor
+
+		std::cout << a;
+		std::cout << b;
+	} catch (const Bureaucrat::GradeTooHighException &myException) {
+		std::cerr << "Exception: " << myException.what() << std::endl;
+	} catch (const Bureaucrat::GradeTooLowException &myException) {
+		std::cerr << "Exception: " << myException.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat c("Ventouse", 150);  // Calls parameterized constructor
+		std::cout << c;
+		c.incrementGrade();
+		std::cout << c;
+		c.decrementGrade();
+		std::cout << c;
+		c.incrementGrade(140);
+		std::cout << c;
+		// c.decrementGrade(); // uncomment to have an error while signing (expect "grade is too low" error)
+		Form form1("Destroy the company?", 10, 10);
+		std::cout << "Form title: "<< form1.getName() << std::endl;
+		std::cout << (form1.getIsSigned() ? "The doc is" GREEN " signed" RESET : "The doc is " RED "NOT signed" RESET) << std::endl;
+		std::cout << "To signed this doc, employee must be at least grade: " CYAN << form1.getSignedGrade() << RESET << std::endl;
+		std::cout << c;
+		std::cout << form1;
+		form1.beSigned(c);
+		form1.beSigned(c);
+		form1.beExecuted(c);
+		std::cout << form1;
+	} catch (const Bureaucrat::GradeTooHighException &myException) {
+		std::cerr << "Exception Bureaucrat: " << myException.what() << std::endl;
+	} catch (const Bureaucrat::GradeTooLowException &myException) {
+		std::cerr << "Exception Bureaucrat: " << myException.what() << std::endl;
+	} catch (const Form::GradeTooHighException &myException){
+		std::cerr << "Exception Form: " << myException.what() << std::endl;
+	} catch (const Form::GradeTooLowException &myException){
+		std::cerr << "Exception Form: " << myException.what() << std::endl;
+	}
+
+	return 0;
+}
