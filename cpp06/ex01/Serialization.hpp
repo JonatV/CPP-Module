079bed8e36b7a2dd17dfe9f948f33dbdef8278dd
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-07 12:45:08 +0100

    Docs: cpp06/ex02: update note for the Serializer class

--- cpp06/ex01/Serialization.hpp
+++ cpp06/ex01/Serialization.hpp
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 15:33:47 by jveirman          #+#    #+#             */
-/*   Updated: 2025/02/26 16:21:51 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/07 12:44:35 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <stdint.h>
 
+
 typedef struct s_Data
 {
 	int		num01;
@@ -23,6 +24,15 @@ typedef struct s_Data
 	char	c;
 	bool	b;
 }	Data;
+
+// info:	This function is use to transform a Data obj with its info
+//			into a int value pointer. We call it serialize.
+//			Then we can convert this value back to the obj it was.
+//			It save spaces and time.
+//			We don't lose the information - it's called Deserialize.
+// Why:		We can use it to send the object over a network and then
+//			recreate it on the other side.
+//			Or to save the object to a database, like a backup. And restore it later.
 
 class Serialization
 {
