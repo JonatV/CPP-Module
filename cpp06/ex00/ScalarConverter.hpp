1155552199e73083b581e5fd63a824ebff6fd6a2
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-07 12:34:09 +0100

    Docs: cpp06/ex00: update note for the scalarConverter class

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -10,7 +10,10 @@
 #define IMP "\033[1;31mimpossible\033[0m"
 #define OVER "\033[1;31moverflow\033[0m"
 
-// utility class
+// info	: Having the destructor and destructor in private prevent the user
+// 		  to be able to instantiate the class.
+// Other way : Creating a pure virtual could define the class abstract thus not instantiable
+// Not orthodox since it is not possible to instantiate thus no copy is possible.
 class ScalarConverter
 {
 	private:
