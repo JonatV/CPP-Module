19ed2bb11a422a2129db798be83409f604ba7408
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-09 18:56:32 +0100

    feat: message when the two input are similar

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,18 +6,21 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 18:42:16 by jveirman          #+#    #+#             */
-/*   Updated: 2025/03/09 18:46:40 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/09 18:55:38 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+#include <iostream>
+
 template <typename Type>
 void	swap(Type &a, Type &b)
 {
 	Type	temp;
-
+	
+	std::cout << "Swap " << a << " with " << b << std::endl;
 	temp = a;
 	a = b;
 	b = temp;
@@ -26,12 +29,22 @@ void	swap(Type &a, Type &b)
 template <typename Type>
 Type	min(Type a, Type b)
 {
+	if (a == b)
+	{
+		std::cout << "Both are equal, returning first value : ";
+		return (a);
+	}
 	return (a < b ? a : b);
 }
 
 template <typename Type>
 Type	max(Type a, Type b)
-{
+{	
+	if (a == b)
+	{
+		std::cout << "Both are equal, returning first value : ";
+		return (a);
+	}
 	return (a > b ? a : b);
 }
 
