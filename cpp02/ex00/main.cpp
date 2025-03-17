baa252cc6809560bd5be86bae7312e0e2337c29c
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-17 14:45:21 +0100

    feat: cpp02/ex00: enhance main function to demonstrate Fixed class behavior

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,20 +6,41 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:52:35 by jveirman          #+#    #+#             */
-/*   Updated: 2024/12/19 16:52:36 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/17 14:44:34 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-int main( void ) {
-	Fixed a;
-	Fixed b( a );
-	Fixed c;
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
 	c = b;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	std::cout << std::endl;
+
+	a.setRawBits(42);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	std::cout << std::endl;
+
+	b.setRawBits(19);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	std::cout << std::endl;
+
+	c.setRawBits(404);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
