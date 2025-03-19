332f18b11e70a6047b18da7d6f6460788bb4e662
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-19 13:06:01 +0100

    refactor: cpp01: change clang++ compiler method for c++

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:51:49 by jveirman          #+#    #+#             */
-/*   Updated: 2024/12/19 16:51:49 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/19 12:59:14 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
 	if (argc == 2)
 	{
 		Harl	harl;
 		harl.complain(argv[1]);
 	}
+	else
+		std::cout << "Use this program with argument.\nAvailable options : DEBUG - INFO - WARNING - ERROR" << std::endl;
 	return (0);
 }
