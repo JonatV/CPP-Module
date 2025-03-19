2ecd5d34a801ee1e28b7f8bf12a0ecd04a533527
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-19 10:14:57 +0100

    fix: cpp01/ex03: initialize weapon in HumanB constructor

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,24 +6,31 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:50:54 by jveirman          #+#    #+#             */
-/*   Updated: 2025/03/17 12:47:23 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:09:28 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 #include <iostream>
 
-HumanB::HumanB(std::string n) : name(n){};
+HumanB::HumanB(std::string n) : name(n)
+{
+	weapon = 0;
+};
+
 HumanB::HumanB(std::string n, Weapon &w) : name(n), weapon(&w){};
+
 HumanB::~HumanB(){};
 
-void	HumanB::attack(){
+void	HumanB::attack()
+{
 	if (this->weapon)
 		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 	else
-		std::cout << this->name << " attacks with their " << "very dangerous hands" << std::endl;
+		std::cout << this->name << " attacks with their very dangerous hands" << std::endl;
 }
 
-void	HumanB::setWeapon(Weapon &w){
+void	HumanB::setWeapon(Weapon &w)
+{
 	weapon = &w;
 }
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,17 +6,19 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:50:43 by jveirman          #+#    #+#             */
-/*   Updated: 2025/03/17 12:49:20 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:13:20 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
+#include "iostream"
 
-int	main(void)
+int main(void)
 {
 	{
+		std::cout << "\e[36m";
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
@@ -24,6 +26,7 @@ int	main(void)
 		bob.attack();
 	}
 	{
+		std::cout << "\e[33m";
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
 		jim.attack();
@@ -33,13 +36,16 @@ int	main(void)
 		jim.attack();
 	}
 	{
+		std::cout << "\e[32m";
 		Weapon club = Weapon("crude spiked club");
-		HumanB Jon("Jon", club);
-		Jon.attack();
-		Jon.setWeapon(club);
-		Jon.attack();
+		Weapon wok = Weapon("oily wok");
+		HumanB Ken("Ken", club);
+		Ken.attack();
+		Ken.setWeapon(wok);
+		Ken.attack();
 		club.setType("some other type of club");
-		Jon.attack();
+		Ken.setWeapon(club);
+		Ken.attack();
 	}
 	return 0;
 }
