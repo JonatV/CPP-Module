50ca3b8df5c61e452f654cc09aa54f6087a465cb
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-19 10:30:44 +0100

    feat: cpp07/ex01: handle undisplayable character

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 17:48:09 by jveirman          #+#    #+#             */
-/*   Updated: 2025/03/13 15:18:51 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:29:54 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,35 @@
 #include <iostream>
 #include <string>
 
+#define P "\e[35m"
+#define Y "\e[33m"
+#define R "\e[31m"
+#define N "\e[0m"
+
 void	damnNumbers(int n)
 {
-	std::cout << "Damn number\t\t[" << n << "]" << std::endl;
+	std::cout << P "Damn number\t\t[" << n << "]" N << std::endl;
 }
 
 void	damnGoodOldChar(char c)
 {
-	std::cout << "A good old damn char\t[" << c << "]" << std::endl;
+	if (c >= 32 && c <= 126)
+		std::cout << R "A good old damn char\t[" << c << "]" N << std::endl;
+	else
+		std::cout << R "A good old damn char\t[Non displayable]" N << std::endl;
 }
 
 void	damnString(std::string s)
 {
-	std::cout << "A long damn string\t[" << s << "]" << std::endl;
+	for (size_t i = 0; i < s.length(); i++)
+	{
+		if (s[i] < 32 || s[i] > 126)
+		{
+			std::cout << Y "A long damn string\t[Non displayable]" N << std::endl;
+			return ;
+		}
+	}
+	std::cout << Y "A long damn string\t[" << s << "]" N<< std::endl;
 }
 
 int	main(void)
@@ -35,6 +51,7 @@ int	main(void)
 	char		c[4] = {'d', 'a', 'm', 'n'};
 	std::string	s[3] = {"Oui", "oui", "baguette"};
 
+	iter(n, sizeof(n) / sizeof(n[0]), damnGoodOldChar);
 	iter(n, sizeof(n) / sizeof(n[0]), damnNumbers);
 	iter(c, sizeof(c) / sizeof(c[0]), damnGoodOldChar);
 	iter(s, sizeof(s) / sizeof(s[0]), damnString);
