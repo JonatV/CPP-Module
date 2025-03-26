74bdd2f2d68161fd0ff942e3e3cf4967300e24ed
JonatV <47326572+JonatV@users.noreply.github.com>
2025-03-26 12:10:48 +0100

    fix: cpp04/ex02: safety checks for out of bounds idea's index and init ideas

--- cpp04/ex02/Brain.cpp
+++ cpp04/ex02/Brain.cpp
@@ -6,21 +6,29 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:56:51 by jveirman          #+#    #+#             */
-/*   Updated: 2024/12/19 16:56:51 by jveirman         ###   ########.fr       */
+/*   Updated: 2025/03/26 12:04:20 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
 #include <iostream>
+#include <sstream>
 
 Brain::Brain() {
 	std::cout << "\e[2mDefault constructor Brain called\e[0m" << std::endl;
+	for (int i = 0; i < 100; i++)
+	{
+		std::stringstream ss;
+		ss << "Idea " << i;
+		this->ideas[i] = ss.str();
+	}
 }
 
 Brain::Brain(const Brain &other) {
 	std::cout << "\e[2mCopy constructor Brain called\e[0m" << std::endl;
-	*this = other;
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = other.ideas[i];
 }
 
 Brain::~Brain() {
@@ -38,9 +46,19 @@ Brain &Brain::operator=(const Brain &other){
 }
 
 std::string Brain::getIdea(int index) const {
-	return this->ideas[index];
+	if (index < 0 || index >= 100)
+	{
+		std::cerr << "Error: Index out of bounds in getIdea" << std::endl;
+		return ("");
+	}
+	return (this->ideas[index]);
 }
 
 void Brain::setIdea(const std::string &idea, int index) {
+	if (index < 0 || index >= 100)
+	{
+		std::cerr << "Error: Index out of bounds in setIdea" << std::endl;
+		return;
+	}
 	this->ideas[index] = idea;
 }
